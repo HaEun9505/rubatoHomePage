52aa3310778182b1c0d51a53ba570a640c76d0b7
HaEun9505 <0104whgkdms@naver.com>
2022-08-03 14:19:41 +0900

    루바토 홈페이지 프로젝트 jsp 생성

---
+++ src/main/java/com/haeun/rubato/RubatoHomePageApplication.java
@@ -0,0 +1,13 @@
+package com.haeun.rubato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubatoHomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubatoHomePageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haeun/rubato/RubatoHomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haeun.rubato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubatoHomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
